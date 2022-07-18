ae3d2d8b884b94ec41862440782a1f3421ea1a6e
boom <nuntipatcpe@gmail.com>
2022-07-18 14:17:47 +0700

    feat : setData and setActive -> About

--- src/component/profile/About.jsx
+++ src/component/profile/About.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+import bio from '../../assets/json/bio.json'
 
 function Experience() {
+
+  const [data,setdata] = useState('about');
+  const [color,setColor] = useState('about');
+ 
+  const setActive = (str)=>{
+    setdata(str);
+    setColor(str);
+  }
+
+
   return (
     <div id="about" className="about">
-      Experience
+      <div className="container">
+
+        <nav className="nav-about">
+          <ul>
+            <li className={color === 'about' ? 'red' : 'back'} tabIndex="0" onClick={()=>{setActive('about')}}> About us</li>
+            <li className={color === 'project' ? 'red' : 'back'} tabIndex="0" onClick={()=>{setActive('project')}}> Project</li>
+          </ul>
+        </nav>
+
+        <div className="data-about">
+          <h1>MY BIOGRAPHY</h1>
+          
+          {bio.filter((item)=>item.id===data).map((item)=>{
+              return (
+                <p key={item.id}>{item.data}</p>
+              )
+            })}
+          {/* <p lang="th">
+           ๆไก้ๆไนกีรั้ๆไนกรๆไ้่กรนย่ๆไยกร่ๆไยergergergergกร่ๆไยกร่ๆไยรก่ฟ
+          </p> */}
+        
+        </div>
+      </div>
     </div>
   );
 }
