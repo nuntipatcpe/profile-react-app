e738fb13ea4f0224b003436b42de42de86eb7f29
boom <nuntipatcpe@gmail.com>
2022-07-18 15:23:46 +0700

    fix : muti data -> About

--- src/component/profile/About.jsx
+++ src/component/profile/About.jsx
@@ -2,53 +2,68 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
-import bio from '../../assets/json/bio.json'
+import bio from "../../assets/json/bio.json";
 
 function Experience() {
+  const [data, setdata] = useState("about");
+  const [color, setColor] = useState("about");
 
-  const [data,setdata] = useState('about');
-  const [color,setColor] = useState('about');
- 
-  const setActive = (str)=>{
+  const setActive = (str) => {
     setdata(str);
     setColor(str);
-  }
-
+  };
 
   return (
     <div id="about" className="about">
       <div className="container">
-
         <nav className="nav-about">
           <ul>
-            <li className={color === 'about' ? 'red' : 'back'} tabIndex="0" onClick={()=>{setActive('about')}}> About us</li>
-            <li className={color === 'project' ? 'red' : 'back'} tabIndex="0" onClick={()=>{setActive('project')}}> Project</li>
+            <li
+              className={color === "about" ? "red" : "back"}
+              tabIndex="0"
+              onClick={() => {
+                setActive("about");
+              }}
+            >
+              {" "}
+              About us
+            </li>
+            <li
+              className={color === "project" ? "red" : "back"}
+              tabIndex="0"
+              onClick={() => {
+                setActive("project");
+              }}
+            >
+              {" "}
+              Project
+            </li>
           </ul>
         </nav>
 
         <div className="data-about">
-
           <div className="topic">
-
             <div className="text">
               <div className="text-item-1">M</div>
               <div className="text-item-2">Y BIOGRAPHY</div>
             </div>
-               
           </div>
-          
+
           <div className="content">
-            {bio.filter((item)=>item.id===data).map((item)=>{
-              return (
-                <p key={item.id}>{item.data}</p>
-              )
-            })}
+            {bio
+              .filter((item) => item.id === data)
+              .map((item) => {
+                return (
+                  <>
+                    <p key={item.id}>{item.data}</p>
+                  </>
+                );
+              })}
           </div>
-          
+
           {/* <p lang="th">
            ๆไก้ๆไนกีรั้ๆไนกรๆไ้่กรนย่ๆไยกร่ๆไยergergergergกร่ๆไยกร่ๆไยรก่ฟ
           </p> */}
-        
         </div>
       </div>
     </div>
