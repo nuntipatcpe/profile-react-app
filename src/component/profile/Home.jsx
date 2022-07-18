c880b2ebf7b889a088b86e4631b95b720468a785
boom <nuntipatcpe@gmail.com>
2022-07-18 10:56:52 +0700

    feat: Home page

--- src/component/profile/Home.jsx
+++ src/component/profile/Home.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import img from "../../assets/imgs/profile.png";
+import { AiFillGithub } from 'react-icons/ai';
+import { MdEmail } from 'react-icons/md';
 
 function Home() {
   return (
     <div id="home" className="home">
       <div className="container">
         <div className="home-item">
-
           <div className="item-1">
-
             <div className="row-1">
               <p className="text-highlight text-small">H</p>
               <p className="text-small">ello...</p>
@@ -27,9 +26,13 @@ function Home() {
           <div className="background">
             <picture></picture>
           </div>
-
         </div>
       </div>
+      <div className="profile-contrack">
+         <i><AiFillGithub className="git-icon"/></i>
+        <div className="email"><MdEmail className="email-icon"/>nuntipat.rukpo@gmail.com</div>
+      </div>
+       
     </div>
   );
 }
