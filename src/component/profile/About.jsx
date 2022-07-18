a6a479c1b168d5c726f40fe0b4dd0d3e5f887dbc
boom <nuntipatcpe@gmail.com>
2022-07-19 00:13:22 +0700

    feat : respon menubar

--- src/component/profile/About.jsx
+++ src/component/profile/About.jsx
@@ -5,45 +5,47 @@ import { useState } from "react";
 import bio from "../../assets/json/bio.json";
 
 function Experience() {
-
-  const [data, setdata] = useState(bio.filter((item)=>item.page==='about'));
+  const [data, setdata] = useState(bio.filter((item) => item.page === "about"));
   const [color, setColor] = useState("about");
 
   const setActive = (str) => {
-    setdata(bio.filter((item)=>item.page===str));
+    setdata(bio.filter((item) => item.page === str));
     setColor(str);
   };
   console.log(data);
 
-
-
   return (
     <div id="about" className="about">
       <div className="container">
-        <nav className="nav-about">
-          <ul>
-            <li
-              className={color === "about" ? "red" : "back"}
-              tabIndex="0"
-              onClick={() => {
-                setActive("about");
-              }}
-            >
-              About us
-            </li>
-            <li
-              className={color === "project" ? "red" : "back"}
-              tabIndex="0"
-              onClick={() => {
-                setActive("project");
-              }}
-            >
-              Project
-            </li>
-          </ul>
-        </nav>
 
-        <div className="data-about">
+        <div className="container-left">
+          <nav className="nav-about">
+            <ul>
+              <li
+                className={color === "about" ? "red" : "back"}
+                tabIndex="0"
+                onClick={() => {
+                  setActive("about");
+                }}
+              >
+                About us
+              </li>
+              <li
+                className={color === "project" ? "red" : "back"}
+                tabIndex="0"
+                onClick={() => {
+                  setActive("project");
+                }}
+              >
+                Project
+               
+              </li>
+            </ul>
+          </nav>
+        </div>
+        
+        <div className="container-rigth">
+  
           <div className="topic">
             <div className="text">
               <div className="text-item-1">M</div>
@@ -52,20 +54,19 @@ function Experience() {
           </div>
 
           <div className="content">
-            {data
-              .map((item) => {
-                return (
-                  <div key={item.id}>
-                    <p>{item.data}</p>
-                  </div>
-                );
-              })}
+            {data.map((item) => {
+              return (
+                <div key={item.id}>
+                  <p>{item.data}</p>
+                </div>
+              );
+            })}
           </div>
 
           {/* <p lang="th">
            ๆไก้ๆไนกีรั้ๆไนกรๆไ้่กรนย่ๆไยกร่ๆไยergergergergกร่ๆไยกร่ๆไยรก่ฟ
           </p> */}
-          
+       
         </div>
       </div>
     </div>
--- src/component/profile/NavBar.jsx
+++ src/component/profile/NavBar.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 function Navbar() {
+  const [click, setClick] = useState(false);
+  // console.log(click)
+
   return (
-    <div className='navbar'>
-       <div className='container'>
-        <div className="nav-item">
-          <p><a href='#home'>NUNTIPAT Rukpo</a></p>
-        <ul>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#resume'>Resume</a></li>
-            <li><a href='#skills'>Skills</a></li>
-            <li><a href='#portfolio'>Portfolio</a></li>
-            <li><a href='#contract'>Contract</a></li>
-        </ul>
+    <div className="navbar">
+      <div className="container">
+        <div className="head-con">
+          <div className="logo">
+            <a href="#home">NUNTIPAT Rukpo</a>
+          </div>
+
+          <ul className={click ? "menu active" : "menu"}>
+            <li>
+              <a href="#about" onClick={()=>setClick(!click)}>About</a>
+            </li>
+            <li>
+              <a href="#resume" onClick={()=>setClick(!click)}>Resume</a>
+            </li>
+            <li>
+              <a href="#skills" onClick={()=>setClick(!click)}>Skills</a>
+            </li>
+            <li>
+              <a href="#portfolio" onClick={()=>setClick(!click)}>Portfolio</a>
+            </li>
+            <li>
+              <a href="#contract" onClick={()=>setClick(!click)}>Contract</a>
+            </li>
+          </ul>
+          <AiOutlineMenu
+            className="logo-menu"
+            onClick={() => setClick(!click)}
+          />
         </div>
+      </div>
     </div>
-    </div>
-   
-  )
+  );
 }
 
-export default Navbar                                         
+export default Navbar;
+
+// import React,{useState} from 'react'
+// import { AiOutlineMenu } from 'react-icons/ai';
+// function Navbar() {
+
+//   const [click,setClick] = useState(false);
+//   // console.log(click)
+
+//   return (
+//     <div className='navbar'>
+//        <div className='container'>
+
+//         <div className="nav-item">
+//           <p className='logo'><a href='#home'>NUNTIPAT Rukpo</a></p>
+//           <AiOutlineMenu className='logo-menu' onClick={()=>setClick(!click)} />
+//         <ul className={click ? "menu-active": "menu"}>
+//             <li><a href='#about' onClick={()=>setClick(!click)} >About</a></li>
+//             <li><a href='#resume' onClick={()=>setClick(!click)}>Resume</a></li>
+//             <li><a href='#skills'onClick={()=>setClick(!click)}>Skills</a></li>
+//             <li><a href='#portfolio' onClick={()=>setClick(!click)}>Portfolio</a></li>
+//             <li><a href='#contract' onClick={()=>setClick(!click)}>Contract</a></li>
+//         </ul>
+//         </div>
+
+//     </div>
+//     </div>
+
+//   )
+// }
+
+// export default Navbar
