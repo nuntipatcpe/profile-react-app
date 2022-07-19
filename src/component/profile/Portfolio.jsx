93feceb422e43600a3e640e390578b893da20119
boom <nuntipatcpe@gmail.com>
2022-07-19 16:26:17 +0700

    feat : add skill java MySkill

--- src/component/profile/Portfolio.jsx
+++ src/component/profile/Portfolio.jsx
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 export class Portfolio extends Component {
   render() {
     return (
-      <div>Portfolio</div>
+      <div className="portfolio">
+        <div className="ciontainer">
+            <header>What i do</header>
+        </div>
+        <div className="imnput">
+            <input type="text" placeholder='Search'/>
+        </div>
+      </div>
     )
   }
 }
