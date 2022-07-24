e9523b783acf73994c9297989a1b02e0306d3eb4
boom <nuntipatcpe@gmail.com>
2022-07-24 15:51:58 +0700

    fix deploy img

--- src/assets/imgs/icons/index.js
+++ src/assets/imgs/icons/index.js
@@ -1,11 +1,21 @@
 import css from './css.png'
+import javascript from './javascript.png'
+import html from './html.png'
+import java from './java.png'
+import react from './react.png'
+import redux from './redux.png'
+import sass from './sass.png'
+import nodeJs from './node-js.png'
+import git from './git.png'
+import github from './github.png'
+import figma from './figma.png'
 
 export const getImg=()=>{
     const imgs = [
         {
           "type": "Language",
           "topic": "Javascript",
-          "path": "/public/assets/javascript.png"
+          "path": javascript
         },
         {
           "type": "Language",
@@ -15,49 +25,49 @@ export const getImg=()=>{
         {
           "type": "Language",
           "topic": "HTML",
-          "path": "/public/assets/html.png"
+          "path": html
         },
         {
           "type": "Language",
           "topic": "java",
-          "path": "/public/assets/java.png"
+          "path": java
         },
       
         {
           "type": "Framework&Libery",
           "topic": "React",
-          "path": "/public/assets/react.png"
+          "path": react
         },
       
         {
           "type": "Framework&Libery",
           "topic": "Redux",
-          "path": "/public/assets/redux.png"
+          "path": redux
         },
         {
           "type": "Framework&Libery",
           "topic": "Sass",
-          "path": "/public/assets/sass.png"
+          "path": sass
         },{
           "type": "Other",
           "topic": "Node",
-          "path": "/public/assets/node-js.png"
+          "path": nodeJs
         },
         
         {
           "type": "Other",
           "topic": "Git",
-          "path": "/public/assets/git.png"
+          "path": git
         },
         {
           "type": "Other",
           "topic": "Github",
-          "path": "/public/assets/github.png"
+          "path": github
         },
         {
           "type": "Other",
           "topic": "Figma",
-          "path": "/public/assets/figma.png"
+          "path": figma
         }
       ]
       
